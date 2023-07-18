8ab1a6c748d4702d3138057d8c6e3ea7ce572221
Wx <wxxxcxx@gmail.com>
2023-07-18 22:33:53 +0800

    .

--- hosts/mx/packages-nur.nix
+++ hosts/mx/packages-nur.nix
@@ -4,13 +4,7 @@
     nixpkgs-nur.nixosModules.nur
   ];
 
-
-  nixpkgs.config.permittedInsecurePackages = [
-    "openssl-1.1.1u"
-  ];
-
   environment.systemPackages = [
-    config.nur.repos.xddxdd.wechat-uos-bin
   ];
 }
 
